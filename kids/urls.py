dc5d02c9616af0183425669ac6d62fc2c201b40b
Toneejake <johnkervinevangelista@gmail.com>
2025-07-31 20:38:01 +0800

    feat: Add Kids Zone app with interactive games

---
+++ kids/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.kids_zone_home, name='kids_zone_home'),
+    path('hazard-house/', views.hazard_house_game, name='hazard_house_game'),
+    path('stop-drop-roll/', views.stop_drop_roll_game, name='stop_drop_roll_game'),
+]
---
+++ kids/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def kids_zone_home(request):
+    return render(request, 'kids/kids_zone.html')
+
+def hazard_house_game(request):
+    return render(request, 'kids/hazard_house.html')
+
+def stop_drop_roll_game(request):
+    return render(request, 'kids/stop_drop_roll.html')
